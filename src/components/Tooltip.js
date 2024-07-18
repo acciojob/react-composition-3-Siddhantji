8571419c60405e215e7118ff030d79c2456d9edd
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-07-18 18:13:50 +0530

    Update Tooltip.js

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,21 +1,27 @@
-import React from "react";
-import "../styles/App.css";
-const Tooltip = ({ children, text }) => {
-  const [visible, setVisible] = useState(false);
+import React, { useState } from "react";
+import './../styles/App.css'; 
 
-  const showTooltip = () => setVisible(true);
-  const hideTooltip = () => setVisible(false);
-
-  return (
-    <div
-      className="tooltip-container"
-      onMouseEnter={showTooltip}
-      onMouseLeave={hideTooltip}
-    >
-      {children}
-      {visible && <div className="tooltip-text">{text}</div>}
-    </div>
-  );
-};
-
-export default Tooltip;
+const Tooltip = ({ text, children }) => {
+    const [showTooltip, setShowTooltip] = useState(false);
+  
+    const handleMouseEnter = () => {
+        console.log("one")
+      setShowTooltip(true);
+    };
+  
+    const handleMouseLeave = () => {
+        console.log("two")
+      setShowTooltip(false);
+    };
+  
+    return (
+      <div className="tooltip-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {/* Apply class name "tooltip" to children element container */}
+        <div >{children}</div>
+        {/* Apply class name "tooltiptext" to tooltip text */}
+        {showTooltip && <div className="tooltiptext">{text}</div>}
+      </div>
+    );
+  };
+  
+  export default Tooltip;
